412bc84ff9af98060ee20cdeb7774d2461a1eef7
朱瑞恒 <18749830459@163.com>
2018-02-03 19:05:06 +0800

    新建

---
+++ pages/indexs/indexs.js
@@ -0,0 +1,21 @@
+// pages/indexs/indexs.js
+Page({
+  data: {
+  
+  },
+  onLoad: function (options) {
+  
+  },
+  onReady: function () {
+  
+  },
+  onShow: function () {
+  
+  },
+  onHide: function () {
+  
+  },
+ onShareAppMessage: function () {
+  
+  }
+})
